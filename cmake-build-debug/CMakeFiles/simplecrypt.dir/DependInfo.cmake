d0d31d23043624728b616fb53a8ae7cf0ef2f702
BuildTools <unconfigured@null.spigotmc.org>
2020-03-24 18:11:01 +0100

    Added decrypting

---
+++ cmake-build-debug/CMakeFiles/simplecrypt.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/dev/simplecrypt/crypting.c" "D:/dev/simplecrypt/cmake-build-debug/CMakeFiles/simplecrypt.dir/crypting.c.obj"
+  "D:/dev/simplecrypt/decrypting.c" "D:/dev/simplecrypt/cmake-build-debug/CMakeFiles/simplecrypt.dir/decrypting.c.obj"
+  "D:/dev/simplecrypt/main.c" "D:/dev/simplecrypt/cmake-build-debug/CMakeFiles/simplecrypt.dir/main.c.obj"
+  "D:/dev/simplecrypt/randgen.c" "D:/dev/simplecrypt/cmake-build-debug/CMakeFiles/simplecrypt.dir/randgen.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/simplecrypt.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simplecrypt.dir/crypting.c.obj"
+  "CMakeFiles/simplecrypt.dir/decrypting.c.obj"
+  "CMakeFiles/simplecrypt.dir/main.c.obj"
+  "CMakeFiles/simplecrypt.dir/randgen.c.obj"
+  "libsimplecrypt.dll.a"
+  "simplecrypt.exe"
+  "simplecrypt.exe.manifest"
+  "simplecrypt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simplecrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
